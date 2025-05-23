0c06e7a99664e9efbf14fd8e93779ebc0b569856
Jordan Dorcelus <example@email.comjdorcelus@pursuit.orggit config --global user.nameJordandorcelus>
2025-05-23 12:39:31 -0400

    Js file has been updated with new event codes

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
 const increment = document.querySelector(".increment");
-increment.addEventListener("click", () => {
-  const hearts = document.querySelector(".hearts");
-  hearts.textContent += "❤";
-});
 
 const decrement = document.querySelector(".decrement");
-decrement.addEventListener("click", () => {
-  const hearts = document.querySelector(".hearts");
-  hearts.textContent = hearts.textContent.slice(0, -1);
+
+const allIncrements = document.querySelectorAll(".increment");
+
+const todoDecrements = document.querySelectorAll(".decrement");
+
+
+let addAndRemoveCheck = false;
+
+function addAndRemoveHandler() {
+  const hearts = document.querySelectorAll(".hearts");
+
+  if(addAndRemoveCheck) return;
+
+  // const allIncrements = document.querySelectorAll(".increment");
+  allIncrements.forEach(plusEle => {
+  plusEle.addEventListener("click", (event) => {
+    const article = event.target.closest("article");
+    const heart = article.querySelector(".hearts");
+    heart.textContent += "❤";
+
+    console.log("event.target:", event.target);
+  });
 });
+
+todoDecrements.forEach(minusEle => {
+  minusEle.addEventListener("click", (event) => {
+    const article = event.target.closest("article");
+    const heart = article.querySelector(".hearts");
+    heart.textContent = heart.textContent.slice(0, -1);
+
+    console.log("event.target:", event.target);
+  });
+});
+
+  addAndRemoveCheck = true;
+
+}
+
+increment.addEventListener("click", addAndRemoveHandler());
+decrement.addEventListener("click", addAndRemoveHandler());
+
+
